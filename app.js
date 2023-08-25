48f4bf3550a73c498f57be0d4eb4f7f210e2b0b1
hammadkhan0313 <141897517+hammadkhan0313@users.noreply.github.com>
2023-08-25 12:59:30 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+alert("welcome to my web" );
+
+alert ("Erro! Please enter a valid passward");  
+
+
+alert ("Welcome to js Land...\n happy coading" )
+
+alert("welcome to JS land");
+
+ alert("Happy coading!\n  prevent this page from craeting additional dialogs.")
+var checkbox= document.querySelectorAll
+
+// var a = 10;
+b= alert ("hello... I can run JS through my web browser console");
+// console.log(b);
